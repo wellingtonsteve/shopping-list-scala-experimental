6bcb7d7ee9e4fe9ae08a76fcaa4fbf5d5c0051e7
Steve Wellington <wellingtonsteve@gmail.com>
2015-07-27 21:44:56 +0100

    Cleanup imports

--- server/server-rest/src/test/scala/uk/co/stevewellington/shoppinglist/RestServerTest.scala
+++ server/server-rest/src/test/scala/uk/co/stevewellington/shoppinglist/RestServerTest.scala
@@ -1,11 +1,11 @@
 package uk.co.stevewellington.shoppinglist
 
 import org.junit.runner.RunWith
+import org.mockito.Mockito.verify
+import org.scalatest.FunSpecLike
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{Inside, Inspectors, FunSpecLike}
 import org.scalatest.mock.MockitoSugar
 import org.scalatra.test.scalatest.ScalatraSuite
-import org.mockito.Mockito.verify
 
 @RunWith(classOf[JUnitRunner])
 class RestServerTest extends ScalatraSuite with FunSpecLike with MockitoSugar {
