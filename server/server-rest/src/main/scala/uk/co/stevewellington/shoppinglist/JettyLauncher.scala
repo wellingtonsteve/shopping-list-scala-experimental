f025694d2de5a6e58c99fd98830b8bbda14b61f2
Steve Wellington <wellingtonsteve@gmail.com>
2015-10-13 21:49:37 +0100

    Convert JettyLauncher to Scala

---
+++ server/server-rest/src/main/scala/uk/co/stevewellington/shoppinglist/JettyLauncher.scala
@@ -0,0 +1,25 @@
+package uk.co.stevewellington.shoppinglist
+
+import java.util.EnumSet._
+import javax.servlet.DispatcherType
+
+import com.google.inject.servlet.GuiceFilter
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.ServletContextHandler
+
+object JettyLauncher {
+
+  val portNumber = 8080
+  val server = new Server(portNumber)
+  val handler = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+
+  handler.addFilter(classOf[GuiceFilter], "/*", allOf(classOf[DispatcherType]))
+  handler.addEventListener(new MyGuiceServletConfig)
+  server.setHandler(handler)
+  
+  def main(args: Array[String]) {
+    server.start()
+    server.join()
+  }
+
+}
